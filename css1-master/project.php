38dc801c4859a450fde270e76d70b0771a95267c
mirzagibran221 <haqimirza21@gmail.com>
2019-12-16 13:09:58 +0700

    baru dari responsive

---
+++ css1-master/project.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Belajar Layout V2</title>
+	<link rel="stylesheet" type="text/css" href="external.css">
+</head>
+<body>
+
+<div id="container" class="clearfix">
+	<div id="gambar" class="clearfix">
+		<div id="header" class="clearfix">
+			<div id="sear">
+				<form action="#" method="POST">
+					<input type="text" name="sear" placeholder="Enter keywords... " >
+					<button type="submit"></button>
+				</form>
+			</div><!--/#sear-->
+			<div id="judul">
+				<img src="image/logo-white.png">
+			</div><!--/#judul-->
+			<div id="menu">
+				<ul>
+					<li id="active"><a href="ujian.html">Home</a></li>
+					<li><a href="about_us.html">About Us</a></li>
+					<li><a href="portfolio.html">portfolio</a></li>
+					<li><a href="block.html">block</a></li>
+					<li><a href="contact_us.html">contact us</a></li>
+				</ul>
+			</div><!--/#menu-->
+		</div><!--/#header-->
+		<div id="text">
+				<p>This background image should be a litle wider as alerge monitors display e.g 1950 px</p><p> The height has been set her to 500px, but can obviously be charged to fitrequirements</p>
+		</div><!--/#text-->
+	</div><!--/#gambar-->
+	<div id="join">
+		<p>Curabitur nulla tellur tor ames a in curabitur pede. Idet millisi eros dis orci congue elis et.</p>
+		<a href="#">JOIN NOW!</a>
+	</div><!--/#join-->
+	<div id="service" class="clearfix">
+		<div class="image">
+			<img src="image/gambar-100x-100.jpg">
+		</div>
+		<div class="servis">
+			<a href="service1.html"> Service 1 </a>
+			<p>Curabitur nulla tellur tor ames a in curabitur pede. Idet millisi eros dis orci congue elis et.</p>
+		</div>
+		<div class="image">
+			<img src="image/gambar-100x-100.jpg">
+		</div>
+		<div class="servis">
+			<a href="service2.html"> Service 2 </a>
+			<p>Curabitur nulla tellur tor ames a in curabitur pede. Idet millisi eros dis orci congue elis et.</p>
+		</div>
+		<div class="image">
+			<img src="image/gambar-100x-100.jpg">
+		</div>
+		<div class="servis">
+			<a href="service3.html"> Service 3 </a>
+			<p>Curabitur nulla tellur tor ames a in curabitur pede. Idet millisi eros dis orci congue elis et.</p>
+		</div>
+	</div><!--/#service-->
+	<div id="garis">
+		<hr> <p>Indonectetus facilis</p> <hr>
+	</div>
+	<div id="main" class="clearfix">
+		<div class="facilis">
+			<div class="icon">
+				<img src="image/gambar-300x170.jpg">
+				<h1>Image Name or Title</h1>
+				<p>Some text or text goes here.</p>
+			</div>
+		</div><!--/#facilis-->
+		<div class="facilis">
+			<div class="icon">
+				<img src="image/gambar-300x170.jpg">
+				<h1>Image Name or Title</h1>
+				<p>Some text or text goes here.</p>
+			</div>
+		</div><!--/#facilis-->
+		<div class="facilis">
+			<div class="icon">
+				<img src="image/gambar-300x170.jpg">
+				<h1>Image Name or Title</h1>
+				<p>Some text or text goes here.</p>
+			</div>
+		</div><!--/#facilis-->
+		<div class="facilis">
+			<div class="icon">
+				<img src="image/gambar-300x170.jpg">
+				<h1>Image Name or Title</h1>
+				<p>Some text or text goes here.</p>
+			</div>
+		</div><!--/#facilis-->
+		<div class="facilis">
+			<div class="icon">
+				<img src="image/gambar-300x170.jpg">
+				<h1>Image Name or Title</h1>
+				<p>Some text or text goes here.</p>
+			</div>
+		</div><!--/#facilis-->
+		<div class="facilis" class="clearfix">
+			<div class="icon">
+				<img src="image/gambar-300x170.jpg">
+				<h1>Image Name or Title</h1>
+				<p>Some text or text goes here.</p>
+			</div>
+		</div><!--/#facilis-->
+	</div><!--/#main-->
+	<div id="bawah" class="clearfix">
+		<div id="contact_us" class="clearfix">
+			<h1>Contact Us</h1>
+			<p>Company Name</p><p>Street Name & Number</p><p>Twon</p><p>Postcode/Zip</p><p>Telp: xxxxx xxxxxxxxxx</p><p>Fax: xxxxx xxxxxxxxxx</p><p>Email: contact@mydomain.com</p>
+		</div>
+		<div id="quick">
+			<h1>Quick Links</h1>
+			<a href="#">Link Text Here</a><hr>
+			<a href="#">Link Text Here</a><hr>
+			<a href="#">Link Text Here</a><hr>
+			<a href="#">Link Text Here</a><hr>
+		</div>
+		<div id="from">
+			<h1>From the Block</h1>
+			<h4>Blog Post Title</h4>
+			<p>Posted by Admin on Sep,30,2017</p>
+			<p>Vestibulumaccumsan egestibulum eu justo</p>
+			<p>convallins augue estas aenean elit intesque</p>
+			<p>set facillispede estibulum.</p>
+			<a href="#">Read more ></a>
+		</div>
+		<div id="grab">
+			<h1>Grap Our Newsletter</h1>
+			<form action="#" method="POST">
+			<p><input type="text" name="name" placeholder="Name"></p>
+			<p><input type="email" name="email" placeholder="Email"></p>
+			<br><button type="submit">Submit</button>
+			</form>
+		</div>
+	</div>
+</div><!--/#container-->
+	<div id="footer" class="clearfix">
+		<p>Copyright &copy; 2017 - All Rights Reserved</p><h1>PondokIT.org</h1>
+	</div>
+</body>
+</html>
